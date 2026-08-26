db6a519788a8645bc13ca820a4bd4bda6d089541
Michele Locati <michele@locati.it>
2026-08-26 18:05:23 +0200

    Pass the export destination via the environment

--- src/repo-exporter.js
+++ src/repo-exporter.js
@@ -1,4 +1,5 @@
 import {exec} from 'node:child_process';
+import {env} from 'node:process';
 import awaitChildProcess from './childprocess-awaiter.js';
 
 /**
@@ -6,9 +7,9 @@ import awaitChildProcess from './childprocess-awaiter.js';
  * @returns {Promise<void>}
  */
 export default async function exportRepository(destinationDirectory) {
-  const spawned = exec(
-    `git archive --format=tar HEAD | tar x -C '${destinationDirectory.replace(/(['$\\])/g, '\\$1')}'`,
-  );
+  const spawned = exec('git archive --format=tar HEAD | tar x -C "$EXPORT_DESTINATION_DIRECTORY"', {
+    env: {...env, EXPORT_DESTINATION_DIRECTORY: destinationDirectory},
+  });
   await awaitChildProcess(spawned, true);
   console.log(`The repository has been exported to ${destinationDirectory}`);
 }
